8e193dbe6b6294322285052731f6a7e9b4f78e4e
noob-eng <80917132+noob-eng@users.noreply.github.com>
2023-04-14 23:49:43 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 echo
 echo "Cloning Android Kernel Tools repo"
 echo
-#git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
-git clone --depth=1 https://github.com/TheStrechh/silont-clang clang
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git clang
+#git clone --depth=1 https://github.com/TheStrechh/silont-clang clang
 
 echo
 echo "AnyKernel3 Repo"
@@ -15,8 +15,8 @@ git clone --depth=1 https://github.com/osm0sis/AnyKernel3.git
 echo
 echo "Cloning Kernel Repo"
 echo
-#git clone --depth=1 --branch lineage-20 https://github.com/LineageOS/android_kernel_xiaomi_surya.git kernel
-git clone --depth=1 --branch silont https://github.com/TheStrechh/android_kernel_xiaomi_surya kernel
+git clone --depth=1 --branch lineage-20 https://github.com/LineageOS/android_kernel_xiaomi_surya.git kernel
+#git clone --depth=1 --branch silont https://github.com/TheStrechh/android_kernel_xiaomi_surya kernel
 
 echo
 echo "Setting up env"
@@ -39,8 +39,8 @@ echo
 echo "Moving to kernel dir"
 echo
 
-#cp surya-docker_defconfig kernel/arch/arm64/configs/
-cp silont_defconfig kernel/arch/arm64/configs/
+cp surya-docker_defconfig kernel/arch/arm64/configs/
+#cp silont_defconfig kernel/arch/arm64/configs/
 cd kernel
 echo $PWD
 
@@ -59,15 +59,15 @@ mkdir -p out
 echo
 echo "Set DEFCONFIG"
 echo 
-#make CC=clang O=out surya-docker_defconfig
-make CC=clang O=out silont_defconfig
+make CC=clang O=out surya-docker_defconfig
+#make CC=clang O=out silont_defconfig
 
 echo
 echo "Build The Good Stuff"
 echo 
 
-#make -j8 O=out CC="ccache clang" AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip Image.gz-dtb
-make -j$(nproc --all) CC=clang CROSS_COMPILE=aarch64-linux-gnu- O=out ARCH=arm64 LLVM=1 Image.gz-dtb
+make -j8 O=out CC="ccache clang" AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip Image.gz-dtb
+#make -j$(nproc --all) CC=clang CROSS_COMPILE=aarch64-linux-gnu- O=out ARCH=arm64 LLVM=1 Image.gz-dtb
 
 
 cd ..
